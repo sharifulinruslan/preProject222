ac60303eea4f1f0e17fe912536bc3859e0d0e357
sharifulinruslan <sharifulinruslanmail@gmail.com>
2025-03-19 11:32:33 +0400

    Создан список из 5 машин

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -2,9 +2,23 @@ package web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class CarController {
+    static List<Car> cars = new ArrayList<>();
+
+    static {
+        cars.add(new Car(001, "Car1", 12));
+        cars.add(new Car(002, "Car2", 900));
+        cars.add(new Car(003, "Car3", 1));
+        cars.add(new Car(004, "Car4", 45600));
+        cars.add(new Car(005, "Car5", 330));
+    }
+
     @GetMapping("/cars")
     public String getCars(){
         return "car";
