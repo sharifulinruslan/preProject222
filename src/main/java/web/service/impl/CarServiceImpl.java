75f6bbf550e49532daa4d4eb2d5196c899139a5b
sharifulinruslan <sharifulinruslanmail@gmail.com>
2025-03-19 14:18:31 +0400

    Добавлен отсутствующий Autowired

--- src/main/java/web/service/impl/CarServiceImpl.java
+++ src/main/java/web/service/impl/CarServiceImpl.java
@@ -1,5 +1,6 @@
 package web.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import web.dao.CarDAO;
 import web.model.Car;
@@ -10,7 +11,7 @@ import java.util.List;
 @Component
 public class CarServiceImpl implements CarService {
     private CarDAO carDAO;
-
+    @Autowired
     public CarServiceImpl(CarDAO carDAO) {
         this.carDAO = carDAO;
     }
